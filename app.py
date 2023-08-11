8da2799ab2ea118a20d51ee581150e492f999f1d
Mubtasim47 <mubtasim.fuad.cs@gmail.com>
2023-08-11 15:39:10 +0600

    changes made

--- app.py
+++ app.py
@@ -12,10 +12,17 @@ def index():
     return render_template('index.html')
 
 @app.route('/process', methods=['POST'])
-def process_csv():
-    if 'file' not in request.files:
-        return 'No file part', 400
-    file = request.files['file']
+def process():
+    file1 = request.files.get('file1')
+    file2 = request.files.get('file2')
+    if file1 and file1.filename != '':
+        return process_csv(file1)
+    elif file2 and file2.filename != '':
+        return process_csv_v2(file2)
+    else:
+        return 'No selected file', 400
+
+def process_csv(file):
     if file.filename == '':
         return 'No selected file', 400
     if file:
@@ -28,7 +35,6 @@ def process_csv():
             output.seek(0)
             return send_file(output, mimetype='text/csv', as_attachment=True, attachment_filename='output.csv')
         elif file_ext == ".xlsx":
-            # Save the uploaded file temporarily
             file.save("temp.xlsx")
             process_xlsx("temp.xlsx")
             return send_file("temp.xlsx", as_attachment=True, download_name="processed_file.xlsx")
@@ -67,6 +73,57 @@ def process_xlsx(filename):
         combination = generate_combinations(target_sum)
         if combination:
             ws[f'C{row}'], ws[f'D{row}'], ws[f'E{row}'], ws[f'F{row}'] = combination
+    wb.save(filename)
+
+def process_csv_v2(file):
+    if file.filename == '':
+        return 'No selected file', 400
+    if file:
+        file_ext = os.path.splitext(file.filename)[1]
+        if file_ext == ".csv":
+            df = pd.read_csv(file)
+            df = process_dataframe_v2(df)
+            output = io.StringIO()
+            df.to_csv(output, index=False)
+            output.seek(0)
+            return send_file(output, mimetype='text/csv', as_attachment=True, attachment_filename='output_v2.csv')
+        elif file_ext == ".xlsx":
+            file.save("temp_v2.xlsx")
+            process_xlsx_v2("temp_v2.xlsx")
+            return send_file("temp_v2.xlsx", as_attachment=True, download_name="processed_file_v2.xlsx")
+        else:
+            return 'Unsupported file type', 400
+
+def process_dataframe_v2(df):
+    for index, row in df.iterrows():
+        target_sum = row['I']
+        combination = generate_combinations_v2(target_sum)
+        if combination:
+            df.at[index, 'C'], df.at[index, 'D'], df.at[index, 'E'], df.at[index, 'F'], df.at[index, 'G'], df.at[index, 'H'] = combination
+    return df
+
+def generate_combinations_v2(target_sum):
+    valid_combinations = []
+    for c in [round(i*0.1, 2) for i in range(9)]:
+        for d in [round(i*0.1, 2) for i in range(9)]:
+            for e in [round(i*0.1, 2) for i in range(9)]:
+                for f in [round(i*0.1, 2) for i in range(9)]:
+                    for g in [round(i*0.1, 2) for i in range(33)]:
+                        for h in [round(i*0.1, 2) for i in range(17)]:
+                            if round(c + d + e + f + g + h, 2) == target_sum:
+                                valid_combinations.append((c, d, e, f, g, h))
+    if valid_combinations:
+        return random.choice(valid_combinations)
+    return None
+
+def process_xlsx_v2(filename):
+    wb = openpyxl.load_workbook(filename)
+    ws = wb.active
+    for row in range(2, ws.max_row + 1):
+        target_sum = ws[f'I{row}'].value
+        combination = generate_combinations_v2(target_sum)
+        if combination:
+            ws[f'C{row}'], ws[f'D{row}'], ws[f'E{row}'], ws[f'F{row}'], ws[f'G{row}'], ws[f'H{row}'] = combination
     wb.save(filename)
 
 if __name__ == '__main__':
